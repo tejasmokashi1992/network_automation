e84839b7a110afceb9248b4f781011042da7cad7
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2020-04-21 12:25:03 +0530

    Update nor_network_audit.py

--- Nornir_scripts/Juniper/nor_network_audit.py
+++ Nornir_scripts/Juniper/nor_network_audit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+import json
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import napalm_get
+from nornir.plugins.tasks.files import write_file
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
 from nornir.core.filter import F
-import json
 
 nr = InitNornir(
     inventory={
@@ -17,13 +17,14 @@ nr = InitNornir(
 )
 
 junos = nr.filter(F(platform="junos"))
-#junos = nr.filter(F(hostname="10.91.2.11"))
+#junos = nr.filter(F(hostname="192.168.1.10"))
 
-def audit(task):
+
+def audit(task, command, first_string):
     try:
-       output = task.run(task=netmiko_send_command, command_string="show configuration | display set | match archival")
+       output = task.run(task=netmiko_send_command, command_string=command)
        #print_result(output)
-       if "transfer-on-commit" not in output.result or "archive-sites" not in output.result:
+       if first_string not in output.result:
            print("Error found on :{0}".format(task.host.hostname))
            with open( 'unconfigured_devices', 'a+' ) as audit_out:
               audit_out.write(task.host.hostname+"\n")
@@ -34,7 +35,10 @@ def audit(task):
         print("ERROR:{0}".format(e))
 
 def main():
-    out=junos.run(task=audit, num_workers=20)
+    command = input("Enter the command in display set: ")
+    first_string = input("Enter string you want to be matched: ")
+    out=junos.run(task=audit, command=command, first_string=first_string, num_workers=20)
+    #print_result(out)
 
 if __name__ == '__main__':
     main()
