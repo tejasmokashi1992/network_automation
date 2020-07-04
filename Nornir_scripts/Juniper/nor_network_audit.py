f7b8b59dbb19a271d851ff2d8d37c5514cf9161a
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2020-07-04 13:04:30 +0530

    Update nor_network_audit.py

--- Nornir_scripts/Juniper/nor_network_audit.py
+++ Nornir_scripts/Juniper/nor_network_audit.py
@@ -21,12 +21,12 @@ junos = nr.filter(F(platform="junos"))
 #junos = nr.filter(F(hostname="192.168.1.10"))
 #junos = nr.filter(F(name="CR_1"))
 
-def audit(task, command, first_string):
+def audit(task, command, find_string):
     try:
        output = task.run(task=netmiko_send_command, command_string=command)
        #print_result(output)
-       if first_string not in output.result:
-           print("Error found on :{0}".format(task.host.hostname))
+       if find_string not in output.result:
+           print("Desired string not found on :{0}".format(task.host.hostname))
            with open( 'unconfigured_devices', 'a+' ) as audit_out:
               audit_out.write(task.host.hostname+"\n")
        else:
@@ -38,8 +38,8 @@ def audit(task, command, first_string):
 def main():
     open("unconfigured_devices", "w").close()
     command = click.prompt("Enter the command in display set")
-    first_string = click.prompt("Enter string you want to be matched")
-    out=junos.run(task=audit, command=command, first_string=first_string, num_workers=20)
+    find_string = click.prompt("Enter string you want to be matched")
+    out=junos.run(task=audit, command=command, find_string=find_string, num_workers=20)
     #print_result(out)
 
 if __name__ == '__main__':
