25373f40065d017d3f268111e44591f4fefaef0d
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2020-08-01 18:28:39 +0530

    Update nor_network_audit.py
    added coloring with rich & click

--- Nornir_scripts/Juniper/nor_network_audit.py
+++ Nornir_scripts/Juniper/nor_network_audit.py
@@ -2,20 +2,12 @@
 
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command, napalm_cli
-#from nornir_scrapli.tasks import send_command as scrapli_send_command
+from nornir_scrapli.tasks import send_command as scrapli_send_command
 from nornir.plugins.functions.text import print_result
 from nornir.core.filter import F
 import click
+from rich import print
 
-nr = InitNornir(
-    inventory={
-        "options": {
-            "host_file": "inventory/hosts.yaml",
-            "group_file": "inventory/groups.yaml",
-            "defaults_file": "inventory/defaults.yaml",
-        }
-    }
-)
 
 #junos = nr.filter(F(platform="junos") & F(role="TOR"))
 #junos = nr.filter(F(hostname="192.168.1.10"))
@@ -28,29 +20,35 @@ def audit(task, command, find_string):
        #output = task.run(task=napalm_cli, commands=list(command))
        #print_result(output)
        if all(element in output.result for element in find_string):
-           print("Done Checking: {0}".format(task.host.hostname))
+           print("[bright_green] Done checking: {0} [/bright_green]".format(task.host.hostname))
        else:
-           print("Desired string not found on: {0}".format(task.host.hostname))
+           print("[bright_red] Desired string not found on: {0} [/bright_red]".format(task.host.hostname))
            with open( 'unconfigured_devices', 'a+' ) as audit_out:
                audit_out.write(task.host.hostname+"\n")
 
     except Exception as e:
         print("Error encounterd on: {0}".format(task.host.hostname))
         print("ERROR:{0}".format(e))
-        
+
 def main():
+    nr = InitNornir(config_file="config.yaml")
     open("unconfigured_devices", "w").close()
-    dev_role = click.prompt("Please enter device type for audit:",
+    print("\n"+"**********"+"[u cyan]Welcome to Network Audit Script[/u cyan]"+"**********"+"\n\n")
+    dev_role = click.prompt(click.style("Enter device type for audit:", fg='yellow'),
             type=click.Choice(['TOR', 'corerouter', 'coreswitch', 'all'],
                 case_sensitive=True))
-    command = click.prompt("Enter the command in display set format")
-    find_string = (click.prompt("Enter single string or comma(,) seperated string you want to be matched")).split(',')
+    command = click.prompt(click.style("Enter the desired command ", fg='bright_magenta'))
+    find_string = click.prompt(click.style("Enter string or list of comma seperated strings to be matched", fg='bright_blue')).split(',')
+    print(50*"#")
+
     if dev_role == "all":
         junos = nr.filter(F(platform="junos"))
         out = junos.run(task=audit, command=command, find_string=find_string, num_workers=20)
+        print(50*"#")
     else:
         junos = nr.filter(F(role="{0}".format(dev_role)))
         out = junos.run(task=audit, command=command, find_string=find_string, num_workers=20)
+        print(50*"#")
     #print_result(out)
 
 if __name__ == '__main__':
