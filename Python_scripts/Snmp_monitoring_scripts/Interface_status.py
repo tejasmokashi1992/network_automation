50c03bab7b7fd18632025628327becd6f6f5477f
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2021-05-06 12:19:49 +0530

    documented

--- Python_scripts/Snmp_monitoring_scripts/Interface_status.py
+++ Python_scripts/Snmp_monitoring_scripts/Interface_status.py
@@ -1,6 +1,10 @@
+
 #!/opt/python/bin/python3
+# Module for getting arguments/inputs from user.
 import argparse
+# Used here for exiting script with status code.
 import sys
+# Module for sending commands to bash shell.    
 import subprocess
 
 STATE_OK=0
@@ -9,7 +13,9 @@ STATE_CRITICAL=2
 def get_status(IP, OID):
     try:
         SNMP_SECRET='75Pubm4t1cSNMP'
+        # command to get required data from device.
         command="snmpget -v2c -c "+str(SNMP_SECRET)+" -OQv "+str(IP)+"  "+str(OID)
+        # Run above command on bash shell with subprocess.
         STATUS=(subprocess.getoutput(command))
 
         if not STATUS:
@@ -17,11 +23,13 @@ def get_status(IP, OID):
             print("CRITICAL:{0}".format(MSG))
             sys.exit(2)
         else:
+            # if interfaces is up exit script with status code 0 ie OK.
             if STATUS == 'up':
                 MSG="Interface is Up"
                 print("OK: {0}".format(MSG))
                 sys.exit(0)
             else:
+            # if interfaces is down exit script with status code 2 ie CRITICAL.
                 MSG="Interface is Down"
                 print("CRITICAL: {0}".format(MSG))
                 sys.exit(2)
@@ -30,12 +38,16 @@ def get_status(IP, OID):
             sys.exit(2)
 
 def main():
+    # create a object from argparse module.
     parser = argparse.ArgumentParser()
+    # make a input option to accept IP address.
     parser.add_argument('--ip')
+    # make a input option to accept SNMP OID.
     parser.add_argument('--oid')
     args = parser.parse_args()
     ip=args.ip
     oid=args.oid
+    # call our get_status function defined above.
     get_status(ip, oid)
 
 if __name__ == '__main__':
