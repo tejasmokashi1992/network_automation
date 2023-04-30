a6d3da9be3443e954a5d9d5f3814e9ef8d970363
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2023-04-30 14:58:32 +0530

    Update create_json_prefix_list.py

--- Python_scripts/basic_python_scripts/create_json_prefix_list.py
+++ Python_scripts/basic_python_scripts/create_json_prefix_list.py
@@ -1,10 +1,11 @@
+ip_list = ['1.1.1.0/12', '2.2.2.0/16', '3.3.3.0/24']
 def create_json_prefix_list():
     final = []
-    for IP in a:
+    for IP in ip_list:
         subnet = IP[-2:]
         prefix_list_json = {
         "IpAddress": IP,
-        "PrefixList":"IPV4_PREFIXES_TRANSITEDGE",
+        "PrefixList":"IPV4_PREFIXES,
         "Comment":'ge {}'.format(subnet),
         "IpAddressWithRule":'{0} ge {1}'.format(IP,subnet)
         }
